052bc171240ed43c45007d6ec7ca24109b7104cd
Jefferson Willian <jefferson.viola40@gmail.com>
2021-06-15 00:05:13 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+
+  state = {
+    purchases: []
+  };
+
+  componentDidMount() {
+    fetch('https://me-frontend-challenge-api.herokuapp.com/orders/1')
+      .then(res => res.json())
+      .then(res => {
+        //console.log(data.header.number)
+        this.setState({
+          purchases: res
+        });
+
+      });
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        <div className="page">
+          <div className="Header">
+            <div className="purchases">
+              <p className="purchase">Purchase Order</p>
+              <p className="number">4510001114</p>
+              <p className="code">Serial ME 11223344</p>
+            </div>
+            <div className="container-data">
+              <div className="buyer">
+                <h1>MTP West Buyer</h1>
+                <p className="buyer-name">Jacksonville Group (Jason Burn)</p>
+                <p className="buyer-email">
+                  <a href="jacksonvillegroup@me.com">jacksonvillegroup@me.com</a> </p>
+                <i>903-575-3050</i>
+              </div>
+              <div className="order">
+                <h1>USD 20000</h1>
+                <p className="status">Need to confirm</p>
+                <p className="create">16/04/2020</p>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    );
+
+  }
+
 }
 
 export default App;
---
+++ src/components/Orders/index.js
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+
+//export const PostCard = ({ title, cover, body, id }) => (
+
+export default class Orders extends Component {
+  render() {
+    return (
+      <div className="post">
+        <div className="post-content">
+          <h1>tesste</h1>
+        </div>
+      </div>
+    );
+
+
+  }
+
+
+}
+
+
+
